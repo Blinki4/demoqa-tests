92d2be2c38864ec5864b57404203c2cd5e51b8ee
Blinki4 <dmitriy.velg@mail.ru>
2025-04-05 13:08:53 +0300

    added first test module

---
+++ src/test/java/SimpleJUnitTest.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.*;
+
+public class SimpleJUnitTest {
+    int result;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("\n###   BeforeAll\n");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("###   AfterAll\n");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("###   BeforeEach");
+        result = getResult();
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("###   AfterEach\n");
+    }
+
+    private int getResult() {
+        return 3;
+    }
+
+    @Test
+    void firstTest() {
+        int result = getResult();
+        System.out.println("###   First Test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("###   Second Test");
+        Assertions.assertTrue(result > 2);
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("###   Third Test");
+        Assertions.assertTrue(result > 2);
+    }
+}
