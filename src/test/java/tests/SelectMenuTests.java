62f10adb2a05aaa70ad9b509df00d06469356030
Blinki4 <dmitriy.velg@mail.ru>
2025-04-05 21:40:25 +0300

    added SelectMenuTests

---
+++ src/test/java/tests/SelectMenuTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelectMenuTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void selectMenuTest () {
+        open("/select-menu");
+        $("#withOptGroup").click();
+        $(byText("A root option")).click();
+        $(byText("Select Title")).click();
+        $(byText("Mr.")).click();
+    }
+}
