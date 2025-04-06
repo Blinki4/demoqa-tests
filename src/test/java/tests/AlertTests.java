26906f0798a0586ba65f4fbb203330a1550145b1
Blinki4 <dmitriy.velg@mail.ru>
2025-04-06 15:30:56 +0300

    added Alert Tests

---
+++ src/test/java/tests/AlertTests.java
@@ -0,0 +1,59 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AlertTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void alertTest() {
+        open("/alerts");
+        $("#alertButton").click();
+        dismiss();
+    }
+
+    @Test
+    void confirmTest() {
+        open("/alerts");
+        $("#confirmButton").click();
+        confirm();
+
+        $("#confirmResult").shouldHave(text("Ok"));
+    }
+
+    @Test
+    void dismissTest() {
+        open("/alerts");
+        $("#confirmButton").click();
+        dismiss();
+
+        $("#confirmResult").shouldHave(text("Cancel"));
+    }
+
+    @Test
+    void promptTest() {
+        open("/alerts");
+        $("#promtButton").click();
+        prompt("Dmitriy");
+        $("#promptResult").shouldHave(text("Dmitriy"));
+    }
+
+    @Test
+    void alertWithTimeoutTest() {
+        open("/alerts");
+        $("#timerAlertButton").click();
+        sleep(5000);
+        confirm();
+    }
+}
--- src/test/java/tests/SelectMenuTests.java
+++ src/test/java/tests/SelectMenuTests.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class SelectMenuTests {
     @BeforeAll
